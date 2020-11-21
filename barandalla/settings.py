abc4179ac32114f8dfb0d392e8e90944df59d474
dv <dv@tyler.localdomain>
2020-11-21 04:08:44 +0100

    veeenga

--- barandalla/settings.py
+++ barandalla/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',    
-    'sorl.thumbnail',
+    'django.contrib.staticfiles',
     'gallery.apps.GalleryConfig',
 ]
 
